f89fd705584cfbc3d22fc25f9f374bfb4eb5a701
yhpark <pyh8837@naver.com>
2021-06-03 10:17:07 +0900

    추가 - Post 수정 조회 기능 추가
    PostApiController
    PostsService
    PostUpdateRequestDto
    PostsResponseDto
    Posts
    
    테스트 코드 및 실제 검증 완료
    1. 실제 검증 시 h2 dbms 웹 콘솔 사용(application.properties 수정)
    작성자 : yhpark

--- src/main/java/com/yhpark/springaws/service/posts/PostsService.java
+++ src/main/java/com/yhpark/springaws/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.yhpark.springaws.service.posts;
 
+import com.yhpark.springaws.domain.posts.Posts;
 import com.yhpark.springaws.domain.posts.PostsRepository;
+import com.yhpark.springaws.web.dto.PostsResponseDto;
 import com.yhpark.springaws.web.dto.PostsSaveRequestDto;
+import com.yhpark.springaws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,19 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    public PostsResponseDto findById (Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
+
+        return new PostsResponseDto(entity);
     }
 }
